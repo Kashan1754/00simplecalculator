982208284d0cd932359c4e339371c8573bb83726
Kashan1754 <167319612+Kashan1754@users.noreply.github.com>
2024-04-17 17:55:36 +0500

    Add files via upload

---
+++ kash.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "Enter 1st number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter 2nd number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "Select one of these operators to perform any operations",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subtraction",
+            "Multiplication", "Division"],
+    },
+]);
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber +
+        answer.secondNumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(answer.firstNumber -
+        answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber *
+        answer.secondNumber);
+}
+else if (answer.operators === "Division") {
+    console.log(answer.firstNumber /
+        answer.secondNumber);
+}
+else {
+    console.log("Please select a valid operator...");
+}
+;
